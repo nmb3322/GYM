8ebcce5467a26ff499259194f8b22e9b26cd42ed
박건우 <@LAPTOP-IR08DL2U>
2022-06-13 00:00:53 +0900

    Create : Project

---
+++ GYM/src/main/java/com/gym/GYM/board/dao/BoardDAO.java
@@ -0,0 +1,24 @@
+package com.gym.GYM.board.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gym.GYM.board.dto.BoardDTO;
+
+@Mapper
+public interface BoardDAO {
+
+	List<BoardDTO> boardList();
+
+	int boardRegist(BoardDTO board);
+
+	BoardDTO boardView(int boardNo);
+
+	int boardModify(BoardDTO board);
+
+	void count(int boardNo);
+
+	int boardDelete(int boardNo);
+
+}
